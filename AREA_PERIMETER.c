b8aff26d8f26da29005b5993ccecf8b02d2e5226
nithya-rgb <nithya.yejju@gmail.com>
2026-08-13 12:44:51 +0530

    Implement area and perimeter calculation
    Fix variable types in scanf and correct perimeter calculation.

---
+++ AREA_PERIMETER.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+float length, breadth, area, perimeter;
+
+  int a;
+printf("enter length:");
+scanf("%d", &length);
+printf("enter breadth:");
+scanf("%d", &breadth);
+area = length*breadth;
+perimeter = 2*(length+breadth);
+printf("area=%.2f\n", area);
+printf("perimeter=%.2f\n" , perimeter);
+return 0;
+}
+  
