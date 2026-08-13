9eebc1f8a94e0b799c92bf53781177d81826ffbe
nithya-rgb <nithya.yejju@gmail.com>
2026-08-13 12:53:43 +0530

    Create radius.c

---
+++ radius.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+float area,radius,circumference;
+float pi=3.14;
+ 
+printf("enter radius:");
+scanf("%d", &radius);
+area = pi*radius*radius;
+circumference = 2*pi*radius;
+printf("area=%.2f\n", area);
+printf("circumference=%.2f\n" , circumference);
+return 0;
+}
