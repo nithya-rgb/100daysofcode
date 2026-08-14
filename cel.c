162db9c59e4f25adbbf5bf1a128b93b2d11cd8a5
nithya-rgb <nithya.yejju@gmail.com>
2026-08-14 23:54:10 +0530

    Create cel.c

---
+++ cel.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+float celsius, Fahrenheit;
+printf("enter celsius:");
+scanf ("%f", &celsius);
+(Fahrenheit = celsius*9/5+32);
+printf("temp in Fahrenheit=%.2f", Fahrenheit);
+return 0;
+}
