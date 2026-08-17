4e2dd929be594287e50fb69936ee61ed1d716f04
nithya-rgb <nithya.yejju@gmail.com>
2026-08-17 12:32:44 +0530

    Create leapyear.c

---
+++ leapyear.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+int a;
+printf("enter a year:");
+scanf("%d",&a);
+if(a%400==0){
+printf("leap year");
+}
+else if(a%100==0){
+printf("not a leap year");
+}
+else if(a%4==0){
+printf("leap year");
+}
+else{
+printf("not a leap year");
+}
+return 0;
+}
