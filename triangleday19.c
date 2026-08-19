ceed8518ced893168d840afadfa7cf93eecd62a4
nithya-rgb <nithya.yejju@gmail.com>
2026-08-20 00:42:17 +0530

    Create triangleday19.c

---
+++ triangleday19.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int side1,side2,side3;
+printf("enter 3 sides:"); 
+scanf("%d %d %d",&side1,&side2,&side3);
+if(side1==side2 && side2==side3){
+printf("equilateral triangle");
+}
+else if(side1==side2 || side2==side3 || side1==side3){
+printf("isosceles");
+}
+else{
+printf("scalene");
+}
+return 0;
+}
