5c87a0e51e912ce2cc9794834985c9c3cf791e36
nithya-rgb <nithya.yejju@gmail.com>
2026-09-17 12:55:34 +0530

    Create day32.2.c

---
+++ day32.2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+ int n, digit, i;
+int count[10] = {0};
+int max = 0, result;
+printf("Enter an integer: ");
+scanf("%d", &n);
+while(n > 0) {
+digit = n % 10;
+ count[digit]++;
+n = n/10;
+}
+for(i = 0; i < 10; i++) {
+if(count[i] > max) {
+max = count[i];
+result = i;
+}
+}
+printf("Digit occurring most = %d", result);
+return 0;
+}
